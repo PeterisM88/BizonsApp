1f8875375dbfdfbc75638a2bcc779b7b56c86426
PeterisM88 <80464530+PeterisM88@users.noreply.github.com>
2022-03-20 15:24:47 +0200

    Uploaded SQL queries
    *Insert all Participants / query
    *Point Calculate / query
    *Final Result Calculate / query

---
+++ SQLqueries/SQLQueryFinalResultCalc.sql
@@ -0,0 +1,40 @@
+﻿-- Query calculates final result for competition
+-- Calculating sum of the points of the 5 best stages
+
+WITH allScores AS (
+   SELECT *, CASE 
+                WHEN Distance_Result = 0 THEN 0
+                ELSE MIN(IIF(Distance_Result > 0, Distance_Result, NULL))  OVER (PARTITION BY [Age_Group], [Race_nr] ORDER BY [Distance_Result] ASC)
+                      * 1000.0 / [Distance_Result]                   
+            END AS Points
+   FROM  [ParticipantForm]
+),
+ rankedParticipantScores AS (
+    SELECT * ,
+            ROW_NUMBER() OVER(PARTITION BY [Age_group], [Start_Number] ORDER BY Points DESC ) AS Row_Num   
+    FROM  allScores 
+),
+ bestParticipantScores AS (
+   SELECT [Age_group],
+          [Start_number],
+          [First_name],
+          [Last_Name],
+          CAST(ROUND( SUM( Points ), 0 ) AS INT)  AS Gala_Rezultati
+   FROM   rankedParticipantScores
+   WHERE  Row_Num <= 5
+   GROUP BY [Age_group], [Start_number], [First_name], [Last_Name]
+),
+ rankedWinners AS (
+    SELECT * , 
+			ROW_NUMBER() OVER(PARTITION BY [Age_group] ORDER BY Gala_Rezultati DESC ) AS Vieta_Grupā
+    FROM  bestParticipantScores
+)
+SELECT [Age_group] AS 'Vecuma grupa',
+	   [Start_number] AS 'Starta numurs',
+	   [First_name] AS Vārds,
+	   [Last_Name] AS Uzvārds,
+	   Gala_Rezultati,
+	   Vieta_Grupā
+FROM   rankedWinners
+--WHERE  Vieta_Grupā <= 3 -- Top 3 winners in group
+ORDER BY Age_Group
---
+++ SQLqueries/SQLQueryPointCalcForRace.sql
@@ -0,0 +1,42 @@
+﻿--CREATE VIEW vPointsByParticipant
+--AS
+
+-- Final results 
+-- Calculates points for each participant in one race or in all races
+WITH cte AS (
+   SELECT * ,
+            MIN(IIF(Distance_Result > 0, Distance_Result, NULL)) OVER (PARTITION BY [Age_Group], [Race_nr] ORDER BY [Distance_Result] ASC) AS Min_Distance
+   FROM  [ParticipantForm]
+),
+  pointsByStartNum AS (
+    SELECT *    
+    FROM (
+           SELECT * ,
+                   CASE WHEN Distance_Result = 0 THEN 0
+                        ELSE CAST(ROUND( [Min_Distance] * 1000.0 / [Distance_Result], 0 ) AS INT)
+                   END AS Points
+           FROM  cte 
+    ) t
+) 
+SELECT [Age_group] AS 'Vecuma grupa',
+       [Start_Number] AS 'Starta numurs',
+       [First_Name] AS Vārds,
+       [Last_Name] AS Uzvārds,
+       [Distance_Result] AS Laiks,
+       [Race_nr] AS Posms, -- If calculating points in multiple races
+	   Points AS Punkti
+FROM   pointsByStartNum 
+ --WHERE [Race_nr] = 3
+-- ORDER BY isn't allowed in views
+  ORDER BY [Age_group], [Race_nr], Points DESC
+
+
+-- DROP VIEW vPointsByParticipant
+
+/*
+SELECT * 
+FROM  vPointsByParticipant
+ORDER BY [Age_group], [Race_nr], Points DESC
+
+*/ 
+
